589b2ad1f4b381252a6726807ac730b00516813d
BorisBanchev <boris.banchev03@gmail.com>
2025-03-15 17:17:07 +0200

    Add exercise 12.16

--- todo-app/todo-backend/util/config.js
+++ todo-app/todo-backend/util/config.js
@@ -1,6 +1,6 @@
 const MONGO_URL =
-  process.env.MONGO_URL || 'mongodb://root:example@localhost:3456/the_database'
-const REDIS_URL = process.env.REDIS_URL || 'redis://localhost:6379'
+  process.env.MONGO_URL || 'mongodb://root:example@mongo:3456/the_database'
+const REDIS_URL = process.env.REDIS_URL || 'redis://redis:6379'
 
 module.exports = {
   MONGO_URL, //: 'mongodb://the_username:the_password@localhost:3456/the_database',
